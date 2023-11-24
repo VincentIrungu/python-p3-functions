909502709ef325683fe9ee5c522644eb45ce0cf6
VincentIrungu <vrugundu001@gmail.com>
2023-11-24 23:37:18 +0300

    Complete Assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print(f"Hello, programmer!")
+    #pass
 
-def greet(name):
-    pass
-
+def greet(name=""):
+    print(f"Hello, {name}!")
+    
+ 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, " + name + "!")
+    
 
 def add(num1, num2):
+    return num1 + num2
     pass
 
 def halve(number):
+    return number * 0.5
     pass
